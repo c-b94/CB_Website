458338d636d939e53fb764151e9413bd4456496d
cedric.code <cedricboudreaux@yahoo.com>
2023-09-18 02:03:50 -0500

    changed logo directory

--- client/src/Components/LoadingPage.tsx
+++ client/src/Components/LoadingPage.tsx
@@ -1,5 +1,5 @@
 
-import logo from "/home/cb-code/projects/CB-Website/client/src/assets/logo.png"
+import logo from "../assets/logo.png"
 export default function LoadingPage() {
     
     window.onload = () =>{
